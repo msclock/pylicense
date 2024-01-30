44d895715027483454f6bc16ebafa3d1b2dc86ed
msclock <msclock@qq.com>
2024-01-30 10:36:51 +0800

    test: refactor test
    Signed-off-by: msclock <msclock@qq.com>

--- tests/test_encryptor_utils.py
+++ tests/test_encryptor_utils.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import base64
+import json
 from unittest.mock import Mock
 
 import pytest
@@ -8,24 +10,24 @@ from pyauthorizer.encryptor.utils import decrypt_with_cipher
 
 
 @pytest.mark.parametrize(
-    ("token_value", "cipher_result", "expected_result"),
+    ("token_value"),
     [
-        (
-            "encrypted_token",
-            b"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9",
-            {"typ": "JWT", "alg": "HS256"},
-        )
+        "value 1",
+        "value 2",
+        "value 3",
     ],
 )
-def test_decrypt_with_cipher_pytest(token_value, cipher_result, expected_result):
+def test_decrypt_with_cipher(token_value):
     # Create a mock token object
     token = Mock()
     token.token = token_value
 
     # Create a mock cipher object
     cipher = Mock()
-    cipher.decrypt.return_value = cipher_result
+    cipher.decrypt.return_value = base64.urlsafe_b64encode(
+        json.dumps(token_value).encode("utf-8")
+    )
 
     decrypted_data = decrypt_with_cipher(token, cipher)
 
-    assert decrypted_data == expected_result
+    assert decrypted_data == token_value
